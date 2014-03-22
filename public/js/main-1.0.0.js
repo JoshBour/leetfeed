7b154996da4538d804064e2a40986c20cddfe5e4
JoshBour <xsystemfreak@hotmail.com>
2014-03-22 17:59:56 +0200

    Added the gameplay feature and refactored most of the stuff.

--- public/js/main-1.0.0.js
+++ public/js/main-1.0.0.js
@@ -1,18 +1,80 @@
 $(function () {
 
-    $("#historySort select").on("change", function (e) {
+    $('#mobileMenuToggle').on('click',function(e){
+       $("header nav").toggle();
+    });
+
+    $(document).on('submit', '#summonerForm', function (e) {
+        var form = $(this);
+        var parent = form.parent();
+        parent.toggleLoadingImage();
+        form.ajaxSubmit({
+            target: '#summonerForm',
+            success: function (responseText) {
+                if (responseText.redirect) {
+                    window.location.href = form.attr('action');
+                }
+                parent.toggleLoadingImage();
+            },
+            type: "post"
+        });
+
+        return false;
+    });
+
+    $('.summonerMeta').on('click', function (e) {
+        if (confirm("Do you really want to remove the summoner from your account?")) {
+            var list = $(this).closest('li');
+            $.ajax({
+                url: baseUrl + '/account/remove-summoner',
+                type: "POST",
+                data: {
+                    "summonerId": list.attr('class').substr(9)
+                }
+            }).success(function (data) {
+                if (data.success == 1){
+                    var ul = list.parent();
+                    if(ul.children().length == 1){
+                        ul.replaceWith($("<div/>",{
+                            "class" : "empty",
+                            "text" : "You haven't added a League of Legends account yet! Add one using the button on the right."
+                        }));
+                    }
+                    list.detach();
+                }
+                addMessage(data.message);
+            }).error(function (jqXHR, textStatus) {
+                addMessage('Something with wrong, please try again.');
+            });
+        }
+    });
+
+    $('#addSummonerToggle').on('click', function (e) {
+        var form = $('#addSummoner');
+        var clone = form.clone();
+        form.addClass('hideAfter').show().focusLight();
+        focusedDiv = form;
+    });
+
+    $('#announcement .remove').on('click', function (e) {
+        $(this).parent().detach();
+        createCookie("shownAnnouncement", "true", 5);
+    });
+
+    $(".moduleSort select").on("change", function (e) {
         var option = $(this);
         var url = option.val();
         window.location.href = url;
     });
 
-    if ($('.module').attr('id') == "random") {
+    if ($('#main .module').attr('id') == "random") {
         $.ajax({
             url: baseUrl + '/feed/get-random-feed'
         }).success(function (data) {
-            console.log("done");
+            console.log(data);
             window.location.href = baseUrl + "/feed/" + data.feedId;
         }).error(function (jqXHR, textStatus) {
+            console.log(status);
             window.location.href = baseUrl + "/random";
         });
     }
@@ -83,7 +145,7 @@ $(function () {
                             "id": "commentList",
                             "html": data
                         }).perfectScrollbar({
-                            "wheelSpeed" : 50
+                            "wheelSpeed": 50
                         }));
                     } else {
                         $('#commentList').append(data).perfectScrollbar("update");
@@ -102,15 +164,12 @@ $(function () {
         var list = $(this);
         if (list.scrollTop() === list.prop('scrollHeight') - list.height()) {
             if (!isScrolled) {
-                isScrolled = true;
                 var category = list.attr('id');
                 var page = parseInt(list.attr('data-page')) + 1;
                 var total = list.attr('data-total');
                 if (page - 1 >= total) {
                     var limitDiv = list.children('.limit-reached');
                     if (limitDiv.length != 0) {
-                        alert('here')
-                        console.log(limitDiv);
                         return;
                     } else {
                         limitDiv = $("<li />", {
@@ -119,6 +178,7 @@ $(function () {
                         }).appendTo(list);
                     }
                 } else {
+                    isScrolled = true;
                     list.toggleLoadingImage();
                     $.ajax({
                         url: baseUrl + '/get-more-feeds/' + category + '/' + page,
@@ -129,6 +189,44 @@ $(function () {
 
                     });
                     list.attr('data-page', page);
+                }
+            }
+        }
+    });
+
+    $('#youtubersFeeds').on("scroll", function (e) {
+        var list = $(this);
+        if (list.scrollTop() === list.prop('scrollHeight') - list.height()) {
+            if (!isScrolled) {
+                isScrolled = true;
+                var nextPage = list.attr('data-token');
+                if(nextPage != "finished"){
+                    var youtuberName = $('#youtubers li.active span').attr('class').substr(9);
+                    list.toggleLoadingImage();
+                    $.ajax({
+                        url: baseUrl + '/feed/get-youtuber-feeds/' + youtuberName + '/false/' + nextPage,
+                        timeout: 20000
+                    }).success(function (data) {
+                        var token = data.nextToken == null ? "finished" : data.nextToken;
+                        list.attr('data-token',token);
+                        // then do a request to get the feeds
+                        $.ajax({
+                            url: baseUrl + '/feed/get-youtuber-feeds/' + youtuberName + '/return/' + nextPage,
+                            timeout: 20000
+                        }).success(function (data) {
+                            list.append(data);
+                            list.toggleLoadingImage();
+                            isScrolled = false;
+                            list.perfectScrollbar("destroy");
+                            list.perfectScrollbar({
+                                "wheelSpeed": 50
+                            });
+                        }).error(function (jqXHR, textStatus) {
+                            console.log(status);
+                        });
+                    }).error(function (jqXHR, textStatus) {
+                        console.log(status);
+                    });
                 }
             }
         }
@@ -148,24 +246,30 @@ $(function () {
             var youtuberName = youtuberSpan.attr('class').substring(9);
             feedsList.slideUp();
             module.toggleLoadingImage();
+
+            // first do a request for the new youtuber's playlist token
             $.ajax({
                 url: baseUrl + '/feed/get-youtuber-feeds/' + youtuberName,
                 timeout: 20000
-            }).done(function (data) {
-                feedsList.html('').append(data).slideDown();
-                feedsList.parent().siblings('h1').html(youtuberSpan.html() + "'s Latest Feeds");
-                module.toggleLoadingImage();
-                isSearchingYoutuberFeeds = false;
-                feedsList.perfectScrollbar("destroy");
-                feedsList.perfectScrollbar({
-                    "wheelSpeed": 50
+            }).success(function (data) {
+                var token = data.nextToken == null ? "finished" : data.nextToken;
+                feedsList.attr('data-token',token);
+                // then do a request to get the feeds
+                $.ajax({
+                    url: baseUrl + '/feed/get-youtuber-feeds/' + youtuberName + '/return',
+                    timeout: 20000
+                }).success(function (data) {
+                    feedsList.find('li').detach();
+                    feedsList.prepend(data).slideDown();
+                    feedsList.parent().siblings('h1').html(youtuberSpan.html() + "'s Latest Feeds");
+                    module.toggleLoadingImage();
+                    isSearchingYoutuberFeeds = false;
+                    feedsList.perfectScrollbar("update");
+                }).error(function (jqXHR, textStatus) {
+                    console.log(status);
                 });
-            }).fail(function (jqXHR, textStatus) {
-                if (textStatus == 'timeout') {
-                    alert('The search has timed out, please refresh the page and try again.');
-                } else {
-                    addMessage('Something with wrong with the game search, please try again.');
-                }
+            }).error(function (jqXHR, textStatus) {
+                console.log(status);
             });
         }, 150);
     });
@@ -191,6 +295,8 @@ $(function () {
             $('body').removeClass('unscrollable');
             if (focusedDiv.attr('id') == 'stage') {
                 focusedDiv.find('iframe').detach();
+                focusedDiv.hide();
+            } else if (focusedDiv.hasClass('hideAfter')) {
                 focusedDiv.hide();
             } else {
                 focusedDiv.detach();
